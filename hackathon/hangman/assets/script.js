d2703dbd6e7f57a7a4eac7dede78f68413f25ee6
Zeinab Roumieh <zeinabroumieh@outlook.com>
2023-06-09 11:09:52 +0100

    linked random word api to button, displays hidden word

--- hackathon/hangman/assets/script.js
+++ hackathon/hangman/assets/script.js
@@ -1,5 +1,3 @@
-const alphabetSection = document.querySelector(".alphabet")
-const alphabetList = document.querySelector(".alphabet ul")
 
 const displayAlphabet = () => {
     const alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
@@ -11,4 +9,42 @@ const displayAlphabet = () => {
     }
 }
 
+const displayWord = (word) => {
+    for (let i=0;i<word.length;i++){
+        const letter = word[i]
+        const blankLetter = document.createElement('p')
+        if (i!==word.length){
+            blankLetter.textContent += "_ "
+        } else {
+            blankLetter.textContent += "_"
+        }
+        hiddenWordList.appendChild(blankLetter)
+    }
+}
+
+const getRandomWord = async () => {
+    try{
+        const resp = await fetch("https://random-word-api.herokuapp.com/word")
+        if (resp.ok) {
+            const word = await resp.json()
+            displayWord(word[0])
+        } else {
+            throw "Error: http status code = " + resp.status
+        }
+    } catch (err){
+        console.log(err)
+        alert("Unable to generate random word")
+    }
+
+}
+
+const randomWordButton = document.querySelector("#randomWord")
+
+const hiddenWordSection = document.querySelector(".hiddenWord")
+const hiddenWordList = document.querySelector(".hiddenWord p")
+
+const alphabetSection = document.querySelector(".alphabet")
+const alphabetList = document.querySelector(".alphabet ul")
+
 displayAlphabet()
+randomWordButton.addEventListener("click", getRandomWord)
