bffbc5d697a870945dc11ddfddd163e54b725894
Zeinab Roumieh <zeinabroumieh@outlook.com>
2023-06-09 13:31:19 +0100

    disabled letter buttons if correct letter chosen

--- hackathon/hangman/assets/script.js
+++ hackathon/hangman/assets/script.js
@@ -1,23 +1,40 @@
 
-const displayAlphabet = () => {
+const displayAlphabet = async (word) => {
+    alphabetList.textContent=""
     const alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
     for (let i=0;i<alphabet.length;i++){
         const letter = alphabet[i]
         const letterButton = document.createElement('BUTTON')
         letterButton.textContent = letter
         alphabetList.appendChild(letterButton)
-        letterButton.addEventListener("click",checkLetter)
+        letterButton.addEventListener("click", () => {
+            let correct = checkLetter(letter,word)
+            if (correct==true){
+                //letterButton.disabled = true
+                alert("correct")
+            } else {
+                letterButton.disabled = true
+                alert ("incorrect")
+            }
+        })
     }
 }
 
-const checkLetter = () => {
-    //check if letter clicked is in word
-    //if yes, show letter
-    //if no
+const checkLetter = (letter,word) => {
+    let blankWord=document.querySelector('.hiddenWord p')
+    word = word.toLowerCase()
+    letter =  letter.toLowerCase()
+    console.log(letter)
+    let idx=word.search(letter)
+    if (idx!==-1){                 
+        blankWord.textContent[idx]=letter.toUpperCase()
+        return true
+    } else {
+        return false
+    }
 }
 
 const displayWord = (word) => {
-    console.log(word)
     for (let i=0;i<word.length;i++){
         const letter = word[i]
         const blankWord = document.createElement('p')
@@ -26,18 +43,19 @@ const displayWord = (word) => {
         } else {
             blankWord.textContent += "_"
         }
-        hiddenWordList.appendChild(blankWord)
-        //console.log(blankWord.textContent[0])
+        hiddenWord.appendChild(blankWord)
     }
 }
 
 const getRandomWord = async () => {
-    hiddenWordList.textContent=""
+    hiddenWord.textContent=""
     try{
         const resp = await fetch("https://random-word-api.herokuapp.com/word")
         if (resp.ok) {
             const word = await resp.json()
             displayWord(word[0])
+            displayAlphabet(word[0])
+            return word[0]
         } else {
             throw "Error: http status code = " + resp.status
         }
@@ -50,10 +68,10 @@ const getRandomWord = async () => {
 const randomWordButton = document.querySelector("#randomWord")
 
 const hiddenWordSection = document.querySelector(".hiddenWord")
-const hiddenWordList = document.querySelector(".hiddenWord p")
+const hiddenWord = document.querySelector(".hiddenWord p")
 
 const alphabetSection = document.querySelector(".alphabet")
 const alphabetList = document.querySelector(".alphabet ul")
 
-displayAlphabet()
 randomWordButton.addEventListener("click", getRandomWord)
+
